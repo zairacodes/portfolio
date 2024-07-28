0e8cf66466497dcddee64ee3400e91b6e14495ec
zairacodes <163322374+zairacodes@users.noreply.github.com>
2024-07-28 21:19:31 +0100

    Initialise Projects component

---
+++ src/components/Main/Projects/Projects.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ProjectCard from "./ProjectCard";
+
+function Projects() {
+  return (
+    <>
+      <div>Projects</div>
+      <div>
+        <ProjectCard />
+      </div>
+    </>
+  );
+}
+
+export default Projects;
