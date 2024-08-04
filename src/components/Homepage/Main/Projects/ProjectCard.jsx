9c0f2e4483697b44b2bd79f8f4225b5c0c0fc76c
zairacodes <163322374+zairacodes@users.noreply.github.com>
2024-08-04 21:26:44 +0100

    Update structure of ProjectCard component

--- src/components/Homepage/Main/Projects/ProjectCard.jsx
+++ src/components/Homepage/Main/Projects/ProjectCard.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import projects from "../../../../assets/projects.json";
 
 function ProjectCard() {
-  return <div>ProjectCard</div>;
+  return (
+    <div>
+      {projects.map((project, index) => (
+        <div key={index}>
+          <h3>{project.title}</h3>
+          <p>{project.description}</p>
+          <img src={project.image} alt={`${project.title} – Preview`} />
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default ProjectCard;
