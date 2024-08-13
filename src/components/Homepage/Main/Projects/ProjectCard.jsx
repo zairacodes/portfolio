308d0a48fc1cd2a02b5e340590f2a3d55d09de62
zairacodes <163322374+zairacodes@users.noreply.github.com>
2024-08-13 21:31:22 +0100

    Update ProjectCard with expandedId state for onClick expand functionality

--- src/components/Homepage/Main/Projects/ProjectCard.jsx
+++ src/components/Homepage/Main/Projects/ProjectCard.jsx
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ProjectCard({ projects }) {
+  const [expandedId, setExpandedId] = useState(null);
+
   return (
-    <div>
+    <div className="projects-container">
       {projects.map((project) => (
-        <div key={project.id} className="projectcard-container">
+        <div
+          key={project.id}
+          className={`projectcard-container ${
+            expandedId === project.id ? "expanded" : ""
+          }`}
+          onClick={() => setExpandedId(project.id)}
+        >
           <img
             className="projectcard-image"
             src={project.image}
             alt={`${project.title} – Preview`}
           />
-          <h3 className="projectcard-title">{project.title}</h3>
-          <p className="projectcard-description">{project.description}</p>
+          <div className="content">
+            <h3 className="projectcard-title">{project.title}</h3>
+            <p className="projectcard-description">{project.description}</p>
+          </div>
         </div>
       ))}
     </div>
