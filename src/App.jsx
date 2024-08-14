da280c87eb8e107b844e9929c3f6d1f1c2a020f4
zairacodes <163322374+zairacodes@users.noreply.github.com>
2024-08-14 20:53:26 +0100

    Add dynamic route for ProjectDetails in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Homepage from "./components/Homepage/Homepage";
+import ProjectDetails from "./components/Homepage/Main/Projects/ProjectDetails";
+import projectsData from "../src/assets/projects.json";
 
 function App() {
   return (
@@ -10,6 +12,10 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route
+          path="/projects/:id"
+          element={<ProjectDetails projects={projectsData} />}
+        />
       </Routes>
       <Footer />
     </div>
