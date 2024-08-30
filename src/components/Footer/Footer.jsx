fb90e737ed893af881bfd735fa0b2b4501fa9eea
zairacodes <163322374+zairacodes@users.noreply.github.com>
2024-08-30 12:56:31 +0100

    Update Footer component with link to Contact page

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Footer.css";
 
 function Footer() {
   return (
     <footer className="footer">
-      <p className="footer-text">&copy; 2024 Zaira. All rights reserved.</p>
+      <p className="footer-text">
+        &copy; 2024 Zaira. All rights reserved •{" "}
+        <Link to="/contact" aria-label="Contact page">
+          Contact
+        </Link>
+      </p>
     </footer>
   );
 }
