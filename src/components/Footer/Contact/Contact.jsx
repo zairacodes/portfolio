0f5c8356734e5fff6740ae142d4b30ed6cf3e3b1
zairacodes <163322374+zairacodes@users.noreply.github.com>
2024-08-30 13:06:44 +0100

    Initialise Contact component

---
+++ src/components/Footer/Contact/Contact.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <section>
+      <h1>Contact Me</h1>
+      <p>LinkedIn here</p>
+      <p>GitHub here</p>
+      <form>Contact Form here</form>
+    </section>
+  );
+}
+
+export default Contact;
