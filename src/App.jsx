00fcc57591f52f78969d521bd5fceb73e171fa74
zairacodes <163322374+zairacodes@users.noreply.github.com>
2024-08-30 13:09:42 +0100

    Add route to Contact page in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Homepage from "./components/Homepage/Homepage";
 import ProjectDetails from "./components/Homepage/Main/Projects/ProjectDetails";
+import Contact from "./components/Footer/Contact/Contact";
 import projectsData from "../src/assets/projects.json";
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
           path="/projects/:id"
           element={<ProjectDetails projects={projectsData} />}
         />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
       <Footer />
     </div>
