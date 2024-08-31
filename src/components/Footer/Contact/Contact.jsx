a35dab6c82b6fb936b29b6cd7003c2c6f6253cf6
zairacodes <163322374+zairacodes@users.noreply.github.com>
2024-08-31 18:52:33 +0100

    Update Contact component with content

--- src/components/Footer/Contact/Contact.jsx
+++ src/components/Footer/Contact/Contact.jsx
@@ -19,7 +19,7 @@ function Contact() {
       .then(
         (result) => {
           setStateMessage(
-            "Thank you so much for reaching out! Your message is on its way – I can't wait to connect with you!"
+            "Thank you so much for reaching out! Your message is on its way – I can't wait to read it!"
           );
           setIsSubmitting(false);
           e.target.reset();
@@ -41,16 +41,53 @@ function Contact() {
 
   return (
     <section className="contact-container">
-      <h1>Contact Me</h1>
-      <p>LinkedIn here</p>
-      <p>GitHub here</p>
-      <form onSubmit={sendEmail}>
+      <h1 className="contact-title">Contact Me</h1>
+      <p className="contact-text">I'd love to hear from you!</p>
+      <p className="contact-text">
+        Feel free to connect with me on{" "}
+        <a href="https://www.linkedin.com/in/zaira-n">
+          <img
+            src="https://img.shields.io/badge/-LinkedIn-blue?style=flat&logo=Linkedin&logoColor=white"
+            alt="LinkedIn badge"
+          />
+        </a>{" "}
+        or check out my projects on{" "}
+        <a href="https://github.com/zairacodes">
+          <img
+            src="https://img.shields.io/badge/-GitHub-black?style=flat&logo=GitHub&logoColor=white"
+            alt="GitHub badge"
+          />
+        </a>
+        .
+      </p>
+      <p className="contact-text">
+        If you have any questions or opportunities you'd like to discuss, or
+        just want to say hi, drop me a message below!
+      </p>
+      <form onSubmit={sendEmail} className="contactform">
         <label htmlFor="user_name">Name</label>
-        <input type="text" id="user_name" name="user_name" required />
+        <input
+          type="text"
+          id="user_name"
+          name="user_name"
+          placeholder="Hey! What's your name?"
+          required
+        />
         <label htmlFor="user_email">Email</label>
-        <input type="email" id="user_email" name="user_email" required />
+        <input
+          type="email"
+          id="user_email"
+          name="user_email"
+          placeholder="How can I reach you?"
+          required
+        />
         <label htmlFor="message">Message</label>
-        <textarea id="message" name="message" required />
+        <textarea
+          id="message"
+          name="message"
+          placeholder="I'm all ears, let's chat!"
+          required
+        />
         <input type="submit" value="Send" disabled={isSubmitting} />
         {stateMessage && <p>{stateMessage}</p>}
       </form>
