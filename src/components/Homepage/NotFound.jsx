9eab3e54152fbbe011fc8d90f85ded633cbc69fe
zairacodes <163322374+zairacodes@users.noreply.github.com>
2024-09-02 19:40:37 +0100

    Implement NotFound component and improve error handling

---
+++ src/components/Homepage/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Main/Projects/Projects.css";
+
+function NotFound() {
+  return (
+    <p className="not-found" role="alert">
+      Oops! Page not found...
+    </p>
+  );
+}
+
+export default NotFound;
